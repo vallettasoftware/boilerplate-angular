4098630f7ed0a5642b1a1937b245079da832f6ac
R.Zhuvagin <rodion.zhuvagin@sharp-dev.net>
2023-12-20 12:34:01 +0300

    Added environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://boilerplateapi.stage.sharp-dev.net/api',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://boilerplateapi.stage.sharp-dev.net/api',
+  locales: ['en'],
+  defaultLocale: 'en',
+};
